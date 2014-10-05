6855cdbbbc103c276b88362932ca3b469174ffc3
tnarla <trushithanarla@gmail.com>
2014-10-05 04:47:14 -0400

    Pushhhhiiie
    aerga

--- MagnetWeb/js/maps.js
+++ MagnetWeb/js/maps.js
@@ -3,6 +3,7 @@ var brownUniversity = new google.maps.LatLng(41.8262, -71.4032);
 var markers = [];
 var MY_MAPTYPE_ID = 'style';
 
+
 function initialize() {
 
   var featureOpts = [
@@ -24,8 +25,7 @@ function initialize() {
       ];
 
   var mapOptions = {
-    zoom: 14,
-    center: brownUniversity,
+    zoom: 16,
     mapTypeControlOptions: {
       mapTypeIds: [google.maps.MapTypeId.ROADMAP, MY_MAPTYPE_ID]
     },
@@ -34,6 +34,27 @@ function initialize() {
 
   map = new google.maps.Map(document.getElementById('center'),
       mapOptions);
+
+  // Try HTML5 geolocation
+  if(navigator.geolocation) {
+    navigator.geolocation.getCurrentPosition(function(position) {
+      var pos = new google.maps.LatLng(position.coords.latitude,
+                                       position.coords.longitude);
+
+      var infowindow = new google.maps.InfoWindow({
+        map: map,
+        position: pos,
+        content: 'Current Location'
+      });
+
+      map.setCenter(pos);
+    }, function() {
+      handleNoGeolocation(true);
+    });
+  } else {
+    // Browser doesn't support Geolocation
+    handleNoGeolocation(false);
+  }
 
   var styledMapOptions = {
     name: 'Style'
@@ -44,7 +65,34 @@ function initialize() {
   map.mapTypes.set(MY_MAPTYPE_ID, customMapType);
   google.maps.event.addListener(map, 'click', function(event) {
     addMarker(event.latLng);
+  google.maps.event.addListener(document.getElementById('study'), 'click', toggleBounce);
   });
+}
+
+function toggleBounce() {
+
+  if (marker.getAnimation() != null) {
+    marker.setAnimation(null);
+  } else {
+    marker.setAnimation(google.maps.Animation.BOUNCE);
+  }
+}
+
+function handleNoGeolocation(errorFlag) {
+  if (errorFlag) {
+    var content = 'Error: The Geolocation service failed.';
+  } else {
+    var content = 'Error: Your browser doesn\'t support geolocation.';
+  }
+
+  var options = {
+    map: map,
+    position: new google.maps.LatLng(60, 105),
+    content: content
+  };
+
+  var infowindow = new google.maps.InfoWindow(options);
+  map.setCenter(options.position);
 }
 
 // Add a marker to the map and push to the array.
@@ -85,56 +133,3 @@ google.maps.event.addDomListener(window, 'load', initialize);
 
 
 ////////////////////////////////////////////////////////////////////////////////////////////////////
-
-
-// var map;
-
-// var MY_MAPTYPE_ID = 'style';
-// var brownUniversity = new google.maps.LatLng(41.8262, -71.4032);
-
-// function initialize() {
-// 	var featureOpts = [
-// 	  {
-// 	    stylers: [
-// 	      { hue: '#555555' },
-// 	    	{ lightness: 50},
-// 	      { visibility: 'simplified' },
-// 	      {gamma: 0.5},
-// 	      { weight: 0.5 }
-// 	    ]
-// 	  },
-// 	  {
-// 	    elementType: 'labels',
-// 	    stylers: [
-// 	      { visibility: 'on' }
-// 	    ]
-// 	  },
-// 	    ];
-
-//   var mapOptions = {
-//     zoom: 12,
-//     center: brownUniversity,
-//     mapTypeIds: [google.maps.MapTypeId.ROADMAP, MY_MAPTYPE_ID]
-//   };
-//     mapTypeId: MY_MAPTYPE_ID
-//   	map = new google.maps.Map(document.getElementById('center'),
-//       mapOptions);
-
-
-//   var customMapType = new google.maps.StyledMapType(featureOpts, styledMapOptions);
-
-//   map.mapTypes.set(MY_MAPTYPE_ID, customMapType);
-
-
-//   var styledMapOptions = {
-//     name: 'style'
-//   };
-
-//   // This event listener will call addMarker() when the map is clicked.
-//   google.maps.event.addListener(map, 'click', function(event) {
-//     addMarker(event.latLng);
-//   });
-// }
-
-
-// google.maps.event.addDomListener(window, 'load', initialize);
